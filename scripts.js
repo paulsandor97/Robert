e8e879153d711b709e6af0c113c3afed31f41b72
paulsandor97 <sandorlari_paul@yahoo.com>
2023-06-29 13:04:41 +0300

    Demo

---
+++ scripts.js
@@ -0,0 +1,54 @@
+const sliderWrapper = document.getElementsByClassName('slider-wrapper')[0];
+const sliderWrapperImagesCount = Array.from(sliderWrapper.children).filter((children) => children.nodeName === 'IMG').length;
+const sliderWrapperStyle = sliderWrapper.style;
+sliderWrapperStyle.left = `0%`;
+
+const modal = document.getElementById('intro-modal');
+modal.show();
+
+const startPresentationButton = document.getElementById('start-presentation-btn');
+startPresentationButton.addEventListener('click', async () => {
+    modal.close();
+    const delayPerImage = 3000;
+    startAudio();
+    await delay(delayPerImage);
+    transitionImages(delayPerImage);
+});
+
+async function transitionImages(delayInMs, currentChildIndexInView = 0) {
+    currentChildIndexInView++;
+    if (currentChildIndexInView === sliderWrapperImagesCount) {
+        sliderWrapper.setAttribute('no-transition', '');
+        await delay(delayInMs / 2);
+        sliderWrapperStyle.left = '0%';
+        currentChildIndexInView = 1;
+        transitionImages(delayInMs, 0);
+        return;
+    }
+
+    sliderWrapper.removeAttribute('no-transition');
+    const lefStyleWithoutPercent = getPercentageValue(sliderWrapperStyle.left);
+    sliderWrapperStyle.left = `${lefStyleWithoutPercent - 100}%`;
+    await delay(currentChildIndexInView + 1 === sliderWrapperImagesCount ? delayInMs / 2 : delayInMs);
+    transitionImages(delayInMs, currentChildIndexInView);
+}
+
+function delay(delayInMs) {
+    console.log(delayInMs)
+    return new Promise((resolve) => setTimeout(resolve, delayInMs));
+}
+
+function getPercentageValue(percentage) {
+    return percentage.substring(0, percentage.length - 1);
+}
+
+function startAudio() {
+    const audio = document.getElementById('playAudio');
+    const promise = audio.play();
+    promise
+        ?.then(() => {})
+        ?.catch(() => {
+            audio.muted = true;
+            audio.play();
+        });
+}
