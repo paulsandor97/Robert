d2879cc0767c79775d63877e08c2279797b1446e
paulsandor97 <sandorlari_paul@yahoo.com>
2023-06-30 10:31:59 +0300

    updated animation code

--- scripts.js
+++ scripts.js
@@ -1,8 +1,3 @@
-const sliderWrapper = document.getElementsByClassName('slider-wrapper')[0];
-const sliderWrapperImagesCount = Array.from(sliderWrapper.children).filter((children) => children.nodeName === 'IMG').length;
-const sliderWrapperStyle = sliderWrapper.style;
-sliderWrapperStyle.left = `0%`;
-
 const modal = document.getElementById('intro-modal');
 modal.show();
 
@@ -10,34 +5,42 @@ const startPresentationButton = document.getElementById('start-presentation-btn'
 startPresentationButton.addEventListener('click', async () => {
     const container = document.getElementById('container');
     container.style.display = 'block';
-    
+
     modal.style.display = 'none';
-    const delayPerImage = 3000;
     startAudio();
+
+    const sliderWrapper = document.getElementsByClassName('slider-wrapper')[0];
+    const sliderWrapperImagesCount = Array.from(sliderWrapper.children).filter((children) => children.nodeName === 'IMG').length;
+    sliderWrapper.style.left = `0%`;
+    const delayPerImage = 3000;
+
     await delay(delayPerImage);
-    transitionImages(delayPerImage);
+    transitionImages(sliderWrapper, sliderWrapperImagesCount, delayPerImage);
 });
 
-async function transitionImages(delayInMs, currentChildIndexInView = 0) {
+async function transitionImages(sliderWrapper, sliderWrapperImagesCount, delayInMs, currentChildIndexInView = 0) {
     currentChildIndexInView++;
+    const sliderWrapperStyle = sliderWrapper.style;
+    
     if (currentChildIndexInView === sliderWrapperImagesCount) {
         sliderWrapper.setAttribute('no-transition', '');
         sliderWrapperStyle.left = '0%';
-        await delay(delayInMs / 2);
-        currentChildIndexInView = 1;
-        transitionImages(delayInMs, 0);
+
+        await delay(sliderWrapper, sliderWrapperImagesCount, delayInMs / 2);
+        transitionImages(sliderWrapper, sliderWrapperImagesCount, delayInMs, 0);
+
         return;
     }
 
     sliderWrapper.removeAttribute('no-transition');
     const lefStyleWithoutPercent = getPercentageValue(sliderWrapperStyle.left);
     sliderWrapperStyle.left = `${lefStyleWithoutPercent - 100}%`;
+    
     await delay(currentChildIndexInView + 1 === sliderWrapperImagesCount ? delayInMs / 2 : delayInMs);
-    transitionImages(delayInMs, currentChildIndexInView);
+    transitionImages(sliderWrapper, sliderWrapperImagesCount, delayInMs, currentChildIndexInView);
 }
 
 function delay(delayInMs) {
-    console.log(delayInMs)
     return new Promise((resolve) => setTimeout(resolve, delayInMs));
 }
 
@@ -47,8 +50,8 @@ function getPercentageValue(percentage) {
 
 function startAudio() {
     const audio = document.getElementById('playAudio');
-    const promise = audio.play();
-    promise
+    audio
+        .play()
         ?.then(() => {})
         ?.catch(() => {
             audio.muted = true;
